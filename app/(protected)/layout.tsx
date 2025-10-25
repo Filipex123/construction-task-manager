0c41d64f856994627b6b4a8996787a6606520a65
Lucas Carvalho Barros <CIANDT\lbarros@lnb034582cps.ciandt.global>
2025-10-24 23:31:37 -0300

    Add Login fix

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,28 +1,23 @@
+// app/(protected)/layout.tsx
 "use client";
 
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import { PageTemplate } from "./components/PageTemplate";
+import { PageTitleProvider } from "../context/PageTitle.context";
+import { SidebarProvider } from "../context/Sidebar.context";
+import  ProtectedLayout  from "./ProtectedLayout/ProtectedLayout";
 
-export default function ProtectedLayout({ children }: { children: React.ReactNode }) {
-  const [loading, setLoading] = useState(true);
-  const router = useRouter();
-
-  useEffect(() => {
-    const logged = localStorage.getItem("logged");
-    if (!logged) {
-      router.push("/login");
-    } else {
-      setLoading(false);
-    }
-  }, [router]);
-
-  if (loading) {
-    return (
-      <div className="flex h-screen items-center justify-center">
-        Carregando...
-      </div>
-    );
-  }
-
-  return <>{children}</>;
+export default function ProtectedSectionLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ProtectedLayout>
+      <SidebarProvider>
+        <PageTitleProvider>
+          <PageTemplate>{children}</PageTemplate>
+        </PageTitleProvider>
+      </SidebarProvider>
+    </ProtectedLayout>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,45 +1,39 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import { PageTemplate } from './(protected)/components/PageTemplate';
-import { PageTitleProvider } from './context/PageTitle.context';
-import { SidebarProvider } from './context/Sidebar.context';
-import './globals.css';
+// app/layout.tsx
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: 'Vital Gestão - Medição',
-  applicationName: 'Vital App - Medição',
-  description: 'App para controle de medição das atividades das obras de construção civil',
+  title: "Vital Gestão - Medição",
+  applicationName: "Vital App - Medição",
+  description:
+    "App para controle de medição das atividades das obras de construção civil",
   icons: {
-    icon: '/favicon.ico',
-    shortcut: '../logo_construtora-vital.png',
-    apple: '../logo_construtora-vital.png',
+    icon: "/favicon.ico",
   },
-  // themeColor: '#ffffff',
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} min-h-screen font-sans antialiased  bg-gray-100`}>
-        <SidebarProvider>
-          <PageTitleProvider>
-            <PageTemplate children={children} />
-          </PageTitleProvider>
-        </SidebarProvider>
+    <html lang="pt-BR">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} min-h-screen font-sans antialiased bg-gray-100`}
+      >
+        {children}
       </body>
     </html>
   );
